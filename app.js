d4663996c93507dec8dfce9e9ec01a16cc065a6b
Madison May <madison.may@students.olin.edu>
2013-10-13 16:41:37 -0400

    twitterproto -> linked

--- app.js
+++ app.js
@@ -45,150 +45,15 @@ app.configure('production', function () {
  * Helpful
  */
 
-function validateUsername (name) {
-  return String(name).substr(0, 15);
-}
-
-function validateTweet (tweet) {
-  return String(tweet).substr(0, 140);
-}
-
-/**
- * Routes
- */
-
-app.get('/', function (req, res) {
-  res.redirect('https://github.com/mobileproto/twitterproto');
-})
-
-app.get('/secret', function (req, res) {
-  db.tweets.find({
-    // published: true
-  }).sort({date: -1}, function (err, docs) {
-    console.log(docs);
-    res.render('index', {
-      title: 'TweetProto',
-      quotes: docs,
-      user: {}
-    });
+app.get('/:uid', function (req, res) {
+  db.students.findOne({uid: req.params.uid}, function (err, student) {
+    if (student) {
+      res.json(student.classes);
+    } else {
+      res.json({});
+    }
   })
 });
-
-app.get('/tweets', function (req, res) {
-  var query = { };
-  if ('q' in req.query) {
-    query.tweet = {$regex: ".*" + req.query.q + ".*"};
-  }
-  db.tweets.find(query).sort({date: -1}, function (err, docs) {
-    res.json({"tweets": docs});
-  })
-});
-
-app.get('/tweets/:id', function (req, res) {
-  db.tweets.findOne({
-    _id: db.ObjectId(req.params.id)
-  }, function (err, doc) {
-    res.json(doc);
-  })
-});
-
-app.del('/tweets/:id', function (req, res) {
-  db.tweets.remove({
-    _id: db.ObjectId(req.params.id)
-  }, function (err) {
-    res.json({"error": err})
-  })
-});
-
-RegExp.escape= function(s) {
-    return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')
-};
-
-app.get('/:username/tweets', function (req, res) {
-  var query = {
-    username: validateUsername(req.params.username)
-  };
-  if ('q' in req.query) {
-    query.tweet = {$regex: new RegExp(".*" + RegExp.escape(req.query.q) + ".*", i)};
-  }
-  db.tweets.find(query).sort({date: -1}).limit(40, function (err, docs) {
-    res.json({"tweets": docs});
-  })
-});
-
-app.get('/:username/following', function (req, res) {
-  db.following.find({
-    username: validateUsername(req.params.username)
-  }, function (err, docs) {
-    res.json({
-      "following": docs.map(function (entry) {
-        return entry.following;
-      }),
-      "detail": docs
-    });
-  })
-});
-
-app.get('/:username/followers', function (req, res) {
-  db.following.find({
-    following: validateUsername(req.params.username)
-  }, function (err, docs) {
-    res.json({
-      "followers": docs.map(function (entry) {
-        return entry.username;
-      }),
-      "detail": docs
-    });
-  })
-});
-
-app.post('/:username/follow', function (req, res) {
-  if (req.body.username) {
-    db.following.findOne({
-      username: validateUsername(req.params.username),
-      following: validateUsername(req.body.username)
-    }, function (err, found) {
-      if (!found) {
-        db.following.save({
-          username: req.params.username,
-          following: req.body.username,
-          date: Date.now()
-        }, res.json.bind(res, {"error": false}));
-      } else {
-        res.json({"error": false})
-      }
-    });
-  } else {
-    res.json({error: true, message: 'Invalid following request'}, 500);
-  }
-})
-
-app.del('/:username/following/:following', function (req, res) {
-  db.following.remove({
-    username: validateUsername(req.params.username),
-    following: validateUsername(req.params.following)
-  }, function (err) {
-    res.json({"error": err})
-  })
-});
-
-app.get('/users', function (req, res) {
-  db.tweets.distinct('username', function (err, names) {
-    res.json({"usernames": names});
-  });
-})
-
-app.post('/:username/tweets', function (req, res) {
-  if (req.body.tweet) {
-    db.tweets.save({
-      tweet: validateTweet(req.body.tweet),
-      username: validateUsername(req.params.username),
-      date: Date.now()
-    }, res.json.bind(res, {"error": false}));
-  } else {
-    res.json({error: true, message: 'Invalid tweet, please specify tweet="...." in the body.'}, 500);
-  }
-})
 
 /**
  * Launch
